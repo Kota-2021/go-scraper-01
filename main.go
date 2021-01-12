bf0c99e9bffc311981ccdc4b7d880fc4724c4d3b
kota-2021 <kota.lalala.2021@gmail.com>
2021-01-12 12:16:53 +0900

    success

--- main.go
+++ main.go
@@ -3,18 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func getListing(listingURL string) []string {
-	var links []string
+type Gdata struct {
+	url  string
+	cont string
+}
+
+func getListing(listingURL string, refererURL string) ([]Gdata, int) {
+	var getdata []Gdata
+	var count int
+
 	//HTTP client with timeout
 	client := &http.Client{
+		//timeout 30 second
 		Timeout: 30 * time.Second,
 	}
+	// create request
 	request, err := http.NewRequest("GET", listingURL, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -25,41 +33,57 @@ func getListing(listingURL string) []string {
 	request.Header.Set("cache-control", "no-cache")
 	request.Header.Set("dnt", "1")
 	request.Header.Set("upgrade-insecure-requests", "1")
-	//request.Header.Set("referer", "https://www.yelp.com/")
-	request.Header.Set("referer", "https://www.google.com/")
+	request.Header.Set("referer", refererURL)
+	// do request with '↑ request headers'
 	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	// 200 is success
 	if resp.StatusCode == 200 {
+		//get body data
 		doc, err := goquery.NewDocumentFromReader(resp.Body)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("doc: ", doc)
-		//doc.Find(".lemon--ul__373c0__1_cxs a").Each(func(i int, s *goquery.Selection) {
-		doc.Find("div.rc div a").Each(func(i int, s *goquery.Selection) {
+		//get a data in body
+		docf := doc.Find(".link-top-line a")
+		//number of searches
+		count = docf.Length()
+		//take out one by one from docf
+		docf.Each(func(i int, s *goquery.Selection) {
+			//get href data
 			link, _ := s.Attr("href")
-			//link = "https://yelp.com/" + link
-			link = "https://www.google.com/" + link
-			fmt.Println("link", link)
-
-			// Make sure you we only fetch correct URL with corresponding title
-			//if strings.Contains(link, "biz/") {
-			if strings.Contains(link, "") {
-				text := s.Text()
-				//fmt.Println("text: ", text)
-				if text != "" && text != "more" { //to avoid unecessary links
-					links = append(links, link)
-				}
-			}
+			// Gdata is struct
+			var gd Gdata
+			// Put in Gdata
+			gd.url = link
+			gd.cont = s.Text()
+			//Gdata put in Array
+			getdata = append(getdata, gd)
 		})
-		tmp := doc.Find("h3")
-		fmt.Println("doc-text: ", tmp.Text())
 	}
-	return links
+	return getdata, count
 }
 func main() {
-	//m := getListing("https://www.yelp.com/search?cflt=mobilephonerepair&find_loc=San+Francisco%2C+CA")
-	m := getListing("https://www.google.com/search?q=golang")
-	fmt.Println(strings.Join(m, "\n"))
+
+	fmt.Println("started")
+
+	set := map[string]string{
+		"url":     "https://www.sitepoint.com/community/",
+		"referer": "https://www.sitepoint.com/",
+	}
+	fmt.Println("set : ", set)
+
+	link, count := getListing(set["url"], set["referer"])
+
+	fmt.Println("count : ", count)
+
+	for _, s := range link {
+		fmt.Println(s.url)
+		fmt.Println(s.cont)
+	}
+
 	fmt.Println("finished")
 }
